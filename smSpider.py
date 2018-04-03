7d69b70cbc2345d937500f6c55c1bd001c549994
zhoujinl <zhoujinl@126.com>
2018-04-03 23:49:20 +0800

    update smSpider

--- smSpider.py
+++ smSpider.py
@@ -3,19 +3,34 @@
 import urllib
 import urllib2
 import re
+import cookielib
 
 page = 1
 url = "http://www.newsmth.net/nForum/#!article/SMIF/1074107?p=20"
-headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36' }
+loginUrl = "http://www.newsmth.net/nForum/user/ajax_login.json"
+headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
+            "Referer":"http://www.newsmth.net/nForum/",
+            "Host":"www.newsmth.net",
+            "Origin":"http://www.newsmth.net",
+            "X-Requested-With":"XMLHttpRequest"}
 
-print(url)
+cookies = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
+postdata = urllib.urlencode({
+            'id':'jalon',
+            'passwd':'513513'
+        })
+request =  urllib2.Request(loginUrl,postdata,headers)
+response = opener.open(request)
+loginMsg = response.read().decode("GBK")
+print(loginMsg)
 
-request = urllib2.Request(url,headers = headers)
-response = urllib2.urlopen(request)
+pageUrl = "http://www.newsmth.net/nForum/article/SMIF/1074107?ajax&p=22&_t=1522769157210"
+response = opener.open(pageUrl)
+pageMsg= response.read().decode("GBK")
+print(pageMsg)
 
-content = response.read().decode("GBK")
 
-print(content)
 
 # pattern = re.compile('<div.*?author">.*?<a.*?<img.*?>(.*?)</a>.*?<div.*?' +
 #                      'content">(.*?)<!--(.*?)-->.*?</div>(.*?)<div class="stats.*?class="number">(.*?)</i>', re.S)
