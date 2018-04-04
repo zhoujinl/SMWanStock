aca4a388df8ffc6325d471c2f57a53a5329eb52d
zhoujinl <zhoujinl@126.com>
2018-04-04 10:08:53 +0800

    del sm.py

--- sm.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-import requests
-proxies = {
-  'http': 'http://10.10.1.10:3128',
-  'https': 'http://10.10.1.10:1080',
-}
-requests.get('https://morvanzhou.github.io/tutorials/data-manipulation/scraping/1-01-understand-website/')
-
